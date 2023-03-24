089d5d84eb2b17a65badfe083c201d4e3771d133
suoncha <vu.pham232001@gmail.com>
2023-03-24 15:31:02 +0700

    test zipped

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,9 @@ import { Unity, useUnityContext} from "react-unity-webgl";
 function App() {
   const { unityProvider, isLoaded, loadingProgression, sendMessage } = useUnityContext({
     loaderUrl: "/game.loader.js",
-    dataUrl: "/game.data",
-    frameworkUrl: "/game.framework.js",
-    codeUrl: "/game.wasm",
+    dataUrl: "/game.data.gz",
+    frameworkUrl: "/game.framework.js.gz",
+    codeUrl: "/game.wasm.gz",
   });
 
   const loadingPercentage = Math.round(loadingProgression * 100);
